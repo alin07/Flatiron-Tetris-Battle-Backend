e652b9dfce210f99e58b5b449aaec8b5f05bbe8b
Al <lin.alice72@gmail.com>
2018-06-05 13:25:32 -0400

    setup websockets

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  User = require('./api/models/userModel'),
-  Room = require('./api/models/roomModel'),
-  bodyParser = require('body-parser');
-const session = require('express-session');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const User = require('./api/models/userModel');
+const Room = require('./api/models/roomModel');
+const bodyParser = require('body-parser');
+
+const WebSocket = require('ws').Server;
+let connectedUsers = [];
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tetrisdb');
@@ -13,14 +15,26 @@ mongoose.connect('mongodb://localhost/Tetrisdb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var roomRoutes = require('./api/routes/roomRoutes'); //importing route
-var userRoutes = require('./api/routes/userRoutes'); //importing route
-roomRoutes(app); //register the route
-userRoutes(app); //register the route
+const roomRoutes = require('./api/routes/roomRoutes');
+const userRoutes = require('./api/routes/userRoutes');
 
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+roomRoutes(app);
+userRoutes(app);
+
+const server = app.listen(port, function(){
+  console.log('Tetris Battle RESTful API server started on: ' + port);
 });
-app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+//WEB SERVER
+const wss = new WebSocket({ server });
+
+//init Websocket ws and handle incoming connect requests
+wss.on('connection', function connection(ws) {
+  console.log("connection ...");
+  //on connect message
+  ws.on('message', function incoming(message) {
+      console.log('received: %s', message);
+      connectedUsers.push(message);
+  });
+  ws.send('message from server at: ' + new Date());
+});
